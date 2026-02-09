94c4cdbfa273d6f15a8ac37fe3f97e91098579c6
LACOMBE-Dorian-2326065aa <dorian.lacombe@etu.univ-amu.fr>
2026-02-09 14:08:39 +0100

    feat: add Competition and Event controllers with CRUD operations

--- back-end/src/Repository/EventRepository.php
+++ back-end/src/Repository/EventRepository.php
@@ -16,6 +16,24 @@ class EventRepository extends ServiceEntityRepository
         parent::__construct($registry, Event::class);
     }
 
+    public function save(Event $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->persist($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+    public function remove(Event $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->remove($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
     //    /**
     //     * @return Event[] Returns an array of Event objects
     //     */
